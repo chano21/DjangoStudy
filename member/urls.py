42a9706e733333df39665d988cab649775e84f90
chano21 <chano22@naver.com>
2021-03-01 01:12:26 +0900

    (feat) post save 기능 수정중

--- member/urls.py
+++ member/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MemberList.as_view(), name="member"),
+    path("list", views.MemberList.as_view(), name="memberlist"),
+    path("", views.MemberOne.as_view(), name="member"),
 ]
--- member/views.py
+++ member/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+
 from django.http import Http404, HttpResponse
 
 from rest_framework.response import Response
@@ -17,6 +18,8 @@ from .serializers import MemberSerializer
 
 
 class MemberList(APIView):
+    queryset = Member.objects
+
     def get_object(self, pk):
         try:
             return Member.objects.get(pk=pk)
@@ -27,15 +30,71 @@ class MemberList(APIView):
         methods=["get"],
         manual_parameters=[Parameter("Member_Header", IN_HEADER, description="Member Header Test", type=TYPE_STRING)],
     )
-    @action(detail=False, methods=["get"], url_path="member")
+    @action(detail=False, methods=["get"], url_path="list")
     def get(self, request, format=None):
         """
         Member List 조회
 
         """
 
-        queryset = Member.objects.all()
+        queryset = self.queryset
 
         serializer = MemberSerializer(queryset, many=True)
 
         return Response(serializer.data, HttpResponse.status_code)
+
+
+class MemberOne(APIView):
+    queryset = Member.objects
+
+    def get_object(self, pk):
+        try:
+            return Member.objects.get(pk=pk)
+        except Member.DoesNotExist:
+            raise Http404
+
+    @swagger_auto_schema(
+        methods=["post"],
+        request_body=MemberSerializer,
+        manual_parameters=[Parameter("member_name", IN_HEADER, type=TYPE_STRING)],
+    )
+    @action(detail=False, methods=["post"], url_path="")
+    def post(self, request, pk=None):
+
+        """
+
+        Member 생성
+
+        """
+        print(request.data)
+        print(request)
+        member_name = request.data["user_name"]
+        print(member_name)
+
+        data = Member(
+            user_name=request.data.user_name,
+            user_grant=request.data.user_grant,
+            user_age=request.data.user_age,
+            user_email=request.data.user_email,
+        )
+        data.save()
+
+        # queryset = self.queryset.create
+
+        #  data = request.put
+        #  login_user = data.get
+
+        #   user_id = models.AutoField(primary_key=True, verbose_name="유저 아이디")
+        #     user_grant = models.CharField(
+        #         choices=MEMBER_GRANT, default="Admin", max_length=20, verbose_name="유저 권한", null=False
+        #     )
+        #     user_name = models.CharField(max_length=24, blank=True, null=True, verbose_name="유저 이름")
+        #     user_age = models.DateTimeField(blank=True, null=True, verbose_name="유저 이름")
+        #     user_email = models.CharField(max_length=20, blank=True, null=True, verbose_name="유저 나이")
+
+        # data = Member(user_grant=login_user,note_data=note_data.get('note_data'),
+        # created_at=datetime.datetime.now(),updated_at=datetime.datetime.now()) data.save()
+
+        #  serializer = MemberSerializer(queryset, many=True)
+
+        return Response(HttpResponse.status_code)
